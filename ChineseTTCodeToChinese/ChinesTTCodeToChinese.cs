22016cda66be74c7de2d0db27648b6f7de3a6e5d
justforsosi <justforsosi@gmail.com>
2017-10-11 10:52:37 +0800

    Add files via upload

---
+++ ChineseTTCodeToChinese/ChinesTTCodeToChinese.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace ChineseTTCodeToChinese
+{
+    public static class ChinesTTCodeToChinese
+    {
+        public static string ConvertToChineseCharacter(string _TTCode, string fileName="")
+        {
+            string _chineseCharacter = string.Empty;
+            //JSon source folder
+            string _jsonFilePath = string.IsNullOrEmpty(fileName)? Directory.GetCurrentDirectory().Replace("\\bin\\Debug", "\\dict") + @"\ctc2hanzi.json" : fileName;
+            Dictionary<string, string> items = LoadJson(_jsonFilePath);
+            //split TT code, delimited by space
+            string[] _listOfCodes = _TTCode.Split(' ');
+            string pattern = @"^[0-9]{4}$";
+            Regex rgx = new Regex(pattern, RegexOptions.IgnoreCase);
+            foreach (string code in _listOfCodes)
+            {
+                if (rgx.IsMatch(code))
+                    //check if it's exactly 4 digits
+                    _chineseCharacter += " " + items[code];
+            }
+            return _chineseCharacter.Trim();
+        }
+        public static Dictionary<string, string> LoadJson(string fileName)
+        {
+            using (StreamReader r = new StreamReader(fileName))
+            {
+                string json = r.ReadToEnd();
+                Dictionary<string,string> items = JsonConvert.DeserializeObject<Dictionary<string, string>>(json);
+                return items;
+            }
+        }
+
+    }
+}
---
+++ ChineseTTTranslation/Program.cs
@@ -0,0 +1,18 @@
+﻿using ChineseTTCodeToChinese;
+using System;
+
+namespace ChineseTTTranslation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName = @"C:\Users\li.zhu\Documents\visual studio 2015\Projects\ChineseTTTranslation\dict\ctc2hanzi.json";
+            string _TTCode = "ABCD 0001 0001";
+            string chinesechar = ChinesTTCodeToChinese.ConvertToChineseCharacter(_TTCode, fileName);
+            string pinyin = chinesechar.ToPinYin();
+            Console.WriteLine(string.Format("TTCode:{0} Pinyin:{1}", _TTCode, pinyin));
+            Console.ReadLine();
+        }
+    }
+}
